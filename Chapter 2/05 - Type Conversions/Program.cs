a1a1117c77a013579bb9372f2cf4e2d54a3e138d
Israel Mendoza <xander_irsl@hotmail.com>
2023-04-14 21:33:21 -0600

    Conversion

---
+++ Chapter 2/05 - Type Conversions/Program.cs
@@ -0,0 +1,25 @@
+﻿/* 
+Implicit conversion happens when:
+
+1. When the compiler can guarantee the conversion will succeed.
+2. No information is lost during the conversion.
+*/
+
+int x = 12345;
+Console.WriteLine(x.GetType());
+// System.Int32
+
+long y = x;
+Console.WriteLine(y.GetType());
+// System.Int64
+
+/* 
+Explicit conversion happens when:
+
+1. When the compiler cannot guarantee the conversion will succeed.
+2. Information may be lost during the conversion.
+*/
+
+short z = (short)y;
+Console.WriteLine(z.GetType());
+// System.Int16
