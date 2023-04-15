731a4f4906ffacc9690ebd6f33a73c053161d265
Israel Mendoza <xander_irsl@hotmail.com>
2023-04-15 13:06:54 -0600

    Nothing new

---
+++ Chapter 2/07 - Null/Program.cs
@@ -0,0 +1,21 @@
+﻿
+/*
+Reference types can be assigned 'null'. 
+This only means that we are not referencing any object in particular.
+*/
+
+Point p1 = null;
+Console.WriteLine(p1 == null);
+// True
+
+/*
+Trying to read the attribute will raise a Runtime error:
+Console.WriteLine(p1.X);
+
+Unhandled exception. System.NullReferenceException: Object reference not set to an instance of an object.
+*/
+
+public class Point {
+    public int X;
+    public int Y;
+}
