34510ffe46abc70656b694cadce84d2653fd8203
Israel Mendoza <israelmendoza@192.168.1.4>
2023-04-20 22:21:38 -0600

    Deconstructor

---
+++ Chapter 3/03 - Deconstructors/Program.cs
@@ -0,0 +1,60 @@
+﻿// Working with the Deconstructor method to get values from the instance.
+
+var rec = new Rectangle(7.5, 10.5);
+rec.PrintDescription();
+// Rectangle with width: 7.5 and height: 10.5 has an area of 78.75
+
+// Deconstructing:
+(double w1, double h1) = rec;
+
+rec.PrintDescription();
+// Rectangle with width: 7.5 and height: 10.5 has an area of 78.75
+
+Console.WriteLine(w1);
+// 7.5
+Console.WriteLine(h1);
+// 10.5
+
+// Deconstructing using the actual method:
+double w2;
+double h2;
+rec.Deconstruct(out w2, out h2);
+
+Console.WriteLine(w2);
+// 7.5
+Console.WriteLine(h2);
+// 10.5
+
+// Deconstructing using the actual method, 
+// but declaring the variables when calling the method:
+rec.Deconstruct(out double w3, out double h3);
+Console.WriteLine(w3);
+// 7.5
+Console.WriteLine(h3);
+// 10.5
+
+class Rectangle
+{
+    public readonly double Width;
+    public readonly double Height;
+
+    public Rectangle(double width, double height) // Constructor
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public double Area() // Method to get the area of the rectangle
+    {
+        return Width * Height;
+    }
+    // Method to print the description of the rectangle:
+    public void PrintDescription() => Console.WriteLine($"Rectangle with width: {Width} and height: {Height} has an area of {Area()}");
+
+    // Deconstruct method:
+    public void Deconstruct(out double width, out double height)
+    {
+        width = Width;
+        height = Height;
+    }
+}
