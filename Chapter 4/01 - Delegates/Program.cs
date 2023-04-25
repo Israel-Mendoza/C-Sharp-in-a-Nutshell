c3858716fc90ae51897099c36b23f81164aa610f
Israel Mendoza <israelmendoza@192.168.1.4>
2023-04-25 16:05:25 -0600

    Delegates

---
+++ Chapter 4/01 - Delegates/Program.cs
@@ -0,0 +1,21 @@
+﻿// Create an instance of delegate and pass sum method as a parameter 
+myDelegate d = new myDelegate(sum);
+
+// Multicast delegate calls difference() method 
+d += difference;
+
+// Pass values to two methods: sum() and difference()
+d(6, 5);
+
+/////////////////////METHODS AND DELEGATE DEFENITIONS////////////////////////
+
+// Method that prints sum of two int numbers 
+void sum(int x, int y) => Console.WriteLine("Sum is: " + (x + y));
+
+// Method that prints difference of two int numbers 
+void difference(int x, int y) => Console.WriteLine("Difference is: " + (x - y));
+
+// Define a delegate of int type 
+delegate void myDelegate(int num1, int num2);
+// Sum is: 11
+// Difference is: 1
