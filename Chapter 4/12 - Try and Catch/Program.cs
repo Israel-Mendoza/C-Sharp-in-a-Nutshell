f19ef135a23b1e3283bb27b8858cf46040f96068
Israel Mendoza <israelmendoza@192.168.1.4>
2023-04-27 00:04:53 -0600

    Error handling

---
+++ Chapter 4/12 - Try and Catch/Program.cs
@@ -0,0 +1,20 @@
+﻿int Divide(int x) => 10 / x;
+
+int result;
+try
+{
+    result = Divide(0);
+}
+catch (DivideByZeroException ex)
+{
+    Console.WriteLine(ex);
+}
+finally
+{
+    result = 0;
+}
+// System.DivideByZeroException: Attempted to divide by zero.
+//    at Program.<<Main>$>g__Divide|0_0(Int32 x) in
+
+Console.WriteLine($"Result: {result}");
+// Result: 0
---
+++ Chapter 4/13 - Throwing ArgumentNullException/Program.cs
@@ -0,0 +1,35 @@
+﻿void PrintString(string? aMessage)
+{
+    if (aMessage == null)
+        throw new ArgumentNullException(nameof(aMessage));
+    else
+        Console.WriteLine(aMessage);
+}
+
+void PrintString2(string? aMessage)
+{
+    // Being so common, there's a better way to throw the error:
+    ArgumentNullException.ThrowIfNull(aMessage);
+    Console.WriteLine(aMessage);
+}
+
+string? myMessage = null;
+
+try
+{
+    PrintString(myMessage);
+}
+catch (ArgumentNullException ex)
+{
+    Console.WriteLine(ex);
+}
+// System.ArgumentNullException: Value cannot be null. (Parameter 'aMessage')
+try
+{
+    PrintString2(myMessage);
+}
+catch (ArgumentNullException ex)
+{
+    Console.WriteLine(ex);
+}
+// System.ArgumentNullException: Value cannot be null. (Parameter 'aMessage')
