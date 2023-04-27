be8dfd004f7e67308217ea4ddc39278d14c2786f
Israel Mendoza <israelmendoza@192.168.1.4>
2023-04-27 15:00:06 -0600

    Nullables

---
+++ Chapter 4/14 - Nullable Value Types/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+We cannot assign a null value to a non-nullable type:
+
+int num1 = null;
+// error CS0037: Cannot convert null to 'int' because it is a non-nullable value type
+*/
+
+// Creating a nullable type out of an int:
+Nullable<int> num1 = null;
+
+// Using the GetValueOrDefault() method on the nullable type:
+Console.WriteLine(num1.GetValueOrDefault());
+// 0
+
+// Another way to create a nullable out of an int:
+int? num2 = null;
+
+// Using the GetValueOrDefault() method on the nullable type:
+Console.WriteLine(num2.GetValueOrDefault());
+// 0
+// We can ask the method to return a default value should the type be null:
+Console.WriteLine(num2.GetValueOrDefault(2020));
+// 2020
+
+int? num3 = 1992;
+
+// Using the GetValueOrDefault() method on the nullable type:
+Console.WriteLine(num3.GetValueOrDefault());
+
+// Testing for value:
+Console.WriteLine(num1.HasValue);
+// False
+Console.WriteLine(num2.HasValue);
+// False
+Console.WriteLine(num3.HasValue);
+// True
+
+// Using the null-coalescing operator:
+int? num4 = num1 ?? num2 ?? num3 ?? 45;
+int? num5 = num1 ?? 12345;
+
+Console.WriteLine(num4);
+// 1992
+Console.WriteLine(num5);
+// 12345
---
+++ Chapter 4/15 - Nullable Reference Types/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+    By default, and before C# 8, only Value Types could be nullables.
+    Reference types were, by default, nullables, but explicit definition
+    made it difficult to debug Null Exceptions.
+
+    From C# 8 onwards, we have the option to ask the IDE to explicitly 
+    request reference types to be set as Nullable Types.
+
+    This is done in the project csproj file:
+    <Nullable>enable</Nullable>
+
+    Benefits:
+
+    + Allows the programmer to clearly show their intent when declaring variables.
+    + Provides protection against null reference exceptions.
+    + The compiler warns you if you dereference a nullable reference when it may be null.
+*/
+
+var p = new Person();
+Console.WriteLine(p.Name?.Length); // If it's not null, get the Length of the property.
+//
+
+p.Name = "Israel";
+Console.WriteLine(p.Name!.Length); // I know what I'm doing! Trust me when I say this is not null!
+// 6
+
+var u1 = new User<int>(12345);
+var u2 = new User<string?>("12345"); // Compiler is giving me a warning already!
+// warning CS8714: The type 'string?' cannot be used as type parameter 'T' in the generic type 
+// or method 'User<T>'. Nullability of type argument 'string?' doesn't match 'notnull' constraint.
+class Person
+{
+    public string? Name { get; set; }
+    public int Age { get; set; }
+}
+
+class User<T> where T : notnull // The Generic Type must be notnullable
+{
+    public string? Name { get; set; }
+    public T ID { get; set; }
+    public User(T ID) { this.ID = ID; }
+}
