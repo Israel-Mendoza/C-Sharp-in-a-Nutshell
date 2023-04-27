38823b6fb4528030c3a0895ad46eb5a8afe75a00
Israel Mendoza <israelmendoza@192.168.1.4>
2023-04-26 23:36:55 -0600

    Lambda and closures

---
+++ Chapter 4/10 - Lambda expressions/Program.cs
@@ -0,0 +1,63 @@
+﻿// Using the method by passing an int, and a lambda expression 
+// that complies with the Func<int,int> type.
+ResultPrinter(12, x => x * x * x);
+// 1728
+
+// Using closures.
+
+// Using the Multiplier function:
+Func<int, int> ByThree = Multiplier(3);
+Func<int, int> ByFive = Multiplier(5);
+
+// Using the counter 
+Func<int> Counter1 = Counter(1);
+Func<int> Counter2 = Counter(11);
+
+// Multipliers closers:
+Console.WriteLine(ByThree(5));
+// 15
+Console.WriteLine(ByFive(5));
+// 25
+
+// Using the counters:
+Console.WriteLine(Counter1());
+// 1
+Console.WriteLine(Counter1());
+// 2
+Console.WriteLine(Counter1());
+// 3
+Console.WriteLine(Counter1());
+// 4
+Console.WriteLine(Counter1());
+// 5
+Console.WriteLine(Counter2());
+// 11
+Console.WriteLine(Counter2());
+// 12
+Console.WriteLine(Counter2());
+// 13
+Console.WriteLine(Counter2());
+// 14
+Console.WriteLine(Counter2());
+// 15
+
+// Method that accepts an int and a delegate of Func<int,int> type:
+void ResultPrinter(int a, Func<int, int> aMethod)
+{
+    int result = aMethod(a);
+    Console.WriteLine(result);
+}
+
+// A method that returns a method that complies with Func<int,int>
+Func<int, int> Multiplier(int a)
+{
+    int Inner(int b) => a * b;
+    return Inner;
+}
+
+// A method that returns a method that complies with Func<int>
+Func<int> Counter(int initialValue)
+{
+    int InnerCounter() => initialValue++;
+    return InnerCounter;
+}
---
+++ Chapter 4/11 - Catching data in a closure/Program.cs
@@ -0,0 +1,37 @@
+﻿// When we catch a variable from inside a loop, 
+// the captured data will refer to the same end value.
+
+Action[] myActions = new Action[5];
+
+for (var i = 0; i < 5; i++)
+{
+    myActions[i] = () => Console.WriteLine(i);
+}
+
+foreach (var action in myActions)
+{
+    action();
+}
+// 5
+// 5
+// 5
+// 5
+// 5
+
+// To avoid this problem, we must "create" a new i variable
+// in every iteration before assign it to the closure:
+for (var i = 0; i < 5; i++)
+{
+    int localIValue = i;
+    myActions[i] = () => Console.WriteLine(localIValue);
+}
+
+foreach (var action in myActions)
+{
+    action();
+}
+// 0
+// 1
+// 2
+// 3
+// 4
