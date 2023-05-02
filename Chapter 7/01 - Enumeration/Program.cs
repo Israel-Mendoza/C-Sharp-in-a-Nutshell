f8020451b9748e3d7fb7910bb9e5820e344359e7
Israel Mendoza <israelmendoza@192.168.1.4>
2023-05-01 23:17:58 -0600

    Collections

---
+++ Chapter 7/01 - Enumeration/Program.cs
@@ -0,0 +1,69 @@
+﻿// IMPLEMENTING IEnumerable and IEnumerator IN A SIMPLE CLASS
+
+using static System.Console;
+using System.Collections;
+
+var myStrColl = new myStringCollection();
+
+// Getting an IEnumerator from the IEnumerable:
+var myEnumerator = myStrColl.GetEnumerator();
+
+// Using the IEnumerator
+while (myEnumerator.MoveNext())
+{
+    Write($"{myEnumerator.Current} ");
+}
+// One Two Three Four Five Six Seven Eight Nine Ten
+WriteLine();
+
+
+// "foreach" calls the GetEnumerator() method to get the object's IEnumerator:
+foreach (var item in myStrColl)
+{
+    Write($"{item} ");
+}
+// One Two Three Four Five Six Seven Eight Nine Ten
+WriteLine();
+
+
+class myStringCollection : IEnumerable, IEnumerator
+{
+    string[] stringHolder = new string[10]
+    {
+        "One",
+        "Two",
+        "Three",
+        "Four",
+        "Five",
+        "Six",
+        "Seven",
+        "Eight",
+        "Nine",
+        "Ten"
+    };
+    int counter = -1;
+
+    public object Current => stringHolder[counter];
+
+    public bool MoveNext()
+    {
+        if (counter == 9)
+        {
+            Reset();
+            return false;
+        }
+        else
+            counter++;
+        return true;
+    }
+
+    public void Reset()
+    {
+        counter = -1;
+    }
+
+    public IEnumerator GetEnumerator()
+    {
+        return (IEnumerator)this;
+    }
+}
---
+++ Chapter 7/02 - Working with Arrays/Program.cs
@@ -0,0 +1,37 @@
+﻿using static System.Console;
+// SEARCHING
+
+var p1 = new Persona("Israel", 31);
+var p2 = new Persona("Suelen", 32);
+var p3 = new Persona("Antonia", 31);
+var p4 = new Persona("Arturo", 74);
+var p5 = new Persona("Ricardo", 31);
+
+Persona[] personas = new Persona[] { p1, p2, p3, p4, p5 };
+
+var idadeDesejada = 32;
+
+bool GetAge31(Persona p) => p.Idade == idadeDesejada;
+
+var resultado = Array.FindLast(personas, GetAge31);
+
+if (resultado == null)
+    WriteLine($"Não tem pessoas com uma idade de {idadeDesejada}.");
+else
+    WriteLine(resultado);
+
+
+class Persona
+{
+    public string? Nome { set; get; }
+    public int Idade { set; get; }
+    public Persona(string Nome, int Idade)
+    {
+        this.Nome = Nome;
+        this.Idade = Idade;
+    }
+    public override string ToString()
+    {
+        return $"Persona chamada '{Nome}' e que tem {Idade} anos.";
+    }
+}
