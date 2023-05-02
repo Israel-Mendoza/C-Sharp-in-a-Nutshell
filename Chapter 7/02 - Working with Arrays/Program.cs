642d520fbdfc10986140cd39da6d971d3f920a30
Israel Mendoza <xander_irsl@hotmail.com>
2023-05-02 15:40:35 -0600

    Array

--- Chapter 7/02 - Working with Arrays/Program.cs
+++ Chapter 7/02 - Working with Arrays/Program.cs
@@ -1,37 +1,69 @@
 ﻿using static System.Console;
+
 // SEARCHING
+/*
+    We can use the Array methods to search for information in an array.
 
-var p1 = new Persona("Israel", 31);
-var p2 = new Persona("Suelen", 32);
-var p3 = new Persona("Antonia", 31);
-var p4 = new Persona("Arturo", 74);
-var p5 = new Persona("Ricardo", 31);
+    We have some methods, like:
+        BinarySearch
+        Find
+        FindAll
+        FindIndex
+        FindLast
+        FindLastIndex
+*/
 
-Persona[] personas = new Persona[] { p1, p2, p3, p4, p5 };
+var p1 = new Person("Israel", 31);
+var p2 = new Person("Sebastian", 32);
+var p3 = new Person("Antonia", 31);
+var p4 = new Person("Moises", 31);
+var p5 = new Person("Arturo", 74);
+var p6 = new Person("Victor", 29);
 
-var idadeDesejada = 32;
+var people = new Person[] { p1, p2, p3, p4, p5, p6 };
 
-bool GetAge31(Persona p) => p.Idade == idadeDesejada;
+var searchedAge = 32;
 
-var resultado = Array.FindLast(personas, GetAge31);
+// Defining the Predicate<T> function to be used by the FindLast:
+bool GetAge31(Person p) => p.Age == searchedAge;
 
-if (resultado == null)
-    WriteLine($"Não tem pessoas com uma idade de {idadeDesejada}.");
+// Using the finding method:
+Person? lastPersonAged31 = Array.FindLast(people, GetAge31);
+Person? personAged29 = Array.Find(people, p => p.Age == 29);
+Person[] peopleAged31 = Array.FindAll(people, p => p.Age == 31);
+
+if (lastPersonAged31 == null)
+    WriteLine($"We don't have people aged {searchedAge}.");
 else
-    WriteLine(resultado);
+    WriteLine(lastPersonAged31);
 
+if (personAged29 == null)
+    WriteLine($"We don't have people aged {searchedAge}.");
+else
+    WriteLine(personAged29);
 
-class Persona
+if (peopleAged31.Length == 0)
+    WriteLine($"We don't have people aged {searchedAge}.");
+else
 {
-    public string? Nome { set; get; }
-    public int Idade { set; get; }
-    public Persona(string Nome, int Idade)
+    WriteLine("\nPeople aged 31:");
+    foreach (Person p in peopleAged31)
     {
-        this.Nome = Nome;
-        this.Idade = Idade;
+        WriteLine(p);
+    }
+}
+
+class Person
+{
+    public string? Name { set; get; }
+    public int Age { set; get; }
+    public Person(string Name, int Age)
+    {
+        this.Name = Name;
+        this.Age = Age;
     }
     public override string ToString()
     {
-        return $"Persona chamada '{Nome}' e que tem {Idade} anos.";
+        return $"Person called '{Name}' is {Age} years old.";
     }
 }
