b0326fc255f22d603f03f8ef0bbd9175f9413d6e
Israel Mendoza <xander_irsl@hotmail.com>
2023-05-02 17:14:02 -0600

    Lists

---
+++ Chapter 7/03 - List<T> and ArrayList/Program.cs
@@ -0,0 +1,54 @@
+﻿using static System.Console;
+
+/*
+    List<T> is generic.
+    ArrayList is nongeneric.
+
+    Unlike with arrays, all interfaces are implemented publicly, 
+    and methods such as Add and Remove are exposed and work as you would expect.
+*/
+
+// Declaring an empty string. 
+var words = new List<string>(); 
+
+// Adding items to the list:
+words.Add("melón");
+words.Add("pera");
+words.Add("manzana");
+words.Add("piña");
+words.AddRange(new string[] {"limón", "guayaba", "aguacate"});
+
+// Inserting and Removing
+words.Insert(0, "sandía");
+words.Remove("aguacate");
+
+foreach(var fruit in words) Write($"{fruit} ");
+// sandía melón pera manzana piña limón guayaba 
+
+// Removing all ending with "-a":
+words.RemoveAll(fruit => fruit.EndsWith('a'));
+
+WriteLine();
+foreach(var fruit in words) Write($"{fruit} ");
+// melón limón 
+
+// Rebuilding
+words.AddRange(new string[] { "guayaba", "sandía", "pera" });
+
+var WORDS = words.ConvertAll(fruit => fruit.ToUpper());
+
+WriteLine();
+foreach(var fruit in WORDS) Write($"{fruit} ");
+// MELÓN LIMÓN GUAYABA SANDÍA PERA 
+
+WriteLine();
+WriteLine(words.Capacity);
+// 8
+WriteLine(words.Count);
+// 5
+
+// SEARCHING USING METHODS
+WriteLine(words.Find(fruit => fruit.StartsWith('s')));
+// sandía
+WriteLine(words.FindLastIndex(fruit => fruit.EndsWith('a')));
+// 4
---
+++ Chapter 7/04 - LinkedList<T>/Program.cs
@@ -0,0 +1,38 @@
+﻿using static System.Console;
+
+/*
+    LinkedList implements IEnumerable<T> and ICollection<T> (and their nongeneric
+    versions), but not IList<T> because access by index is not supported.
+*/
+
+var escala = new LinkedList<string?>();
+
+escala.AddFirst("do");                          // do
+escala.AddLast("si");                           // do - si
+escala.AddAfter(escala.First!, "re");            // do - re - si
+escala.AddAfter(escala.First!.Next!, "mi");       // do - re - mi - si
+escala.AddBefore(escala.Last!, "la");            // do - re - mi - la - si
+escala.AddBefore(escala.Last!.Previous!, "sol");  // do - re - mi - sol - la - si
+escala.AddAfter(escala.First.Next!.Next!, "fa"); // do - re - mi - fa - sol - la - si
+
+foreach(var nota in escala) Write($"{nota} ");
+// do re mi fa sol la si 
+WriteLine();
+
+WriteLine(escala.Count); // 7
+
+string? n = escala.Find("fa")?.Value;
+
+WriteLine(n?.GetType());
+
+var calificaciones = new LinkedList<double>();
+calificaciones.AddFirst(8.5);
+calificaciones.AddLast(5.8);
+calificaciones.AddAfter(calificaciones.First!, 9.5);
+
+foreach(var nota in calificaciones) Write($"{nota} ");
+WriteLine();
+
+double primeraCalif = calificaciones.First!.Value;
+
+WriteLine($"Type: {primeraCalif.GetType()} - Value: {primeraCalif}");
